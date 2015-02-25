4ad89a37b4e37b98ca946a1c718aeb02b2acd77f
jackfjo3 <jackfjo3@gmail.com>
2015-02-25 13:15:35 -0700

    add devise and create user model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,10 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+
+  # defined default url options in your environments files
+  # devise install instruction 1
+  # NEED TO ADD ACTUAL HOST URL 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
